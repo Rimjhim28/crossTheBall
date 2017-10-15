e6a6e5973cfc41d3cbdae04fd78a6a200b2ae3fe
Rimjhim Bhadani <rimjhimbhadani@gmail.com>
2017-10-15 17:38:00 +0530

    Update StartActivity.java

--- app/src/main/java/com/example/android/crosstheball/StartActivity.java
+++ app/src/main/java/com/example/android/crosstheball/StartActivity.java
@@ -1,13 +1,26 @@
 package com.example.android.crosstheball;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+    
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        btnStart = (Button)findViewById(R.id.button_start);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
