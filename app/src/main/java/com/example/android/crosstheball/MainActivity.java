46ddefbb9e7569f1dcdd536c169bb5f7421a5ae5
Rimjhim Bhadani <rimjhimbhadani@gmail.com>
2017-10-31 19:35:25 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/android/crosstheball/MainActivity.java
+++ app/src/main/java/com/example/android/crosstheball/MainActivity.java
@@ -1,21 +1,14 @@
 package com.example.android.crosstheball;
 
-import android.content.Intent;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ActionProvider;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
